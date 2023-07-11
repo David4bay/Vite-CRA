bf87f2797aacdfa2c4713710375aafea70183808
David4bay <david4bay@gmail.com>
2023-07-11 17:32:01 +0100

    added delete functionality

--- src/components/CreateNote.jsx
+++ src/components/CreateNote.jsx
@@ -19,23 +19,24 @@ const CreateNote = ({textInput, setTextInput, notes, setNotes, name, setName}) =
     }
 
     if (name.length > 15) {
-        setName(name.match(/[A-Za-z0-9? ]{15}/));
+        setName(name.match(/[A-Za-z0-9?]{15}/));
     }
     if (textInput.length > 45) {
-        setTextInput(textInput.match(/[A-Za-z0-9 ]{45}/));
+        setTextInput(textInput.match(/[A-Za-z0-9]{45}/));
     }
     
     return (
-        <form style={{display: 'flex', margin: 'auto', flexDirection: 'column'}}>
+        <form style={{display: 'flex', margin: 'auto', flexDirection: 'column'}}  onSubmit={submitNoteHandler}>
             <input placeholder=' Fill in your name here...' style={{marginBottom: '15px', padding: '8px'}} type='text' value={name} onChange={changeNameHandler}  />
             <textarea 
             value={textInput} 
             onChange={userInputHandler} 
             cols="50" 
             rows="15"
+            on
             placeholder=' Fill in your sticky note...'
             ></textarea>
-            <button style={{display: 'block', margin: '10px auto'}} onClick={submitNoteHandler} type='button'>Submit</button>
+            <button style={{display: 'block', margin: '10px auto'}}>Submit</button>
         </form>
     )
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { useState } from 'react';
 import Likes from './Likes';
 import './Note.css';
 
-function Note({note, notes, setNotes}) {
+function Note({ note, notes, setNotes }) {
 
-    const deletenote = (note) => {
-        setNotes(notes.filter((state) => state.id !== note.id))
-    }
-
-    const noteStyle = {
-        display: 'flex', 
+  const noteStyle = {
+    display: 'flex', 
         flexDirection: 'column', 
         gap: '30px', 
         justifyContent: 'center', 
@@ -22,18 +17,24 @@ function Note({note, notes, setNotes}) {
         margin: '5px auto 20px auto',
         padding: '35px',
         borderRadius: '10px'
-    }
+  };
 
-    return (
-        <div style={noteStyle}>
-            <h2 style={{margin: '5px auto'}}>{note.user}</h2>
-            <h3>{note.message}</h3>
-            <div style={{display: 'flex', gap: '25px'}}>
-            <button onClick={() => deletenote(note)} style={{width: '7rem'}}>Delete</button>
-            <Likes />
-            </div>
-        </div>
-    )
+  const deleteNote = note => {
+    setNotes(notes.filter((notice) => notice !== note))
+  }
+
+  return (
+    <div style={noteStyle}>
+      <h2 style={{ margin: '5px auto' }}>{note.user}</h2>
+      <h3>{note.message}</h3>
+      <div style={{ display: 'flex', gap: '25px' }}>
+        <button onClick={() => deleteNote(note)} style={{ width: '7rem' }}>
+          Delete
+        </button>
+        <Likes />
+      </div>
+    </div>
+  );
 }
 
 export default Note;
